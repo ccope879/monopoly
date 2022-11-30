d4d5cec3e966aaed63e88e8ca1c2880f330cb1fe
ccope879 <83920786+ccope879@users.noreply.github.com>
2022-11-29 23:59:36 -0600

    Delete monopolyGUIExample directory

--- monopolyGUIExample/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.monopolyguiexample {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.monopolyguiexample to javafx.fxml;
-    exports com.mycompany.monopolyguiexample;
-}
